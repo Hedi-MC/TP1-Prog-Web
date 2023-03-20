b54d041f13529c0fc09ebacc1a3cb0db46009b70
Hedi Melo Cherni <2030719@cegepmontpetit.ca>
2023-03-20 13:42:37 -0400

    Reformulation des directories

--- obj/Debug/net6.0/TP2-Skyrim.AssemblyInfo.cs
+++ obj/Debug/net6.0/TP2-Skyrim.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Tp2 Prog Web")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TP2-Skyrim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Tp2 Prog Web")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Tp2 Prog Web")]
+[assembly: System.Reflection.AssemblyProductAttribute("TP2-Skyrim")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TP2-Skyrim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
